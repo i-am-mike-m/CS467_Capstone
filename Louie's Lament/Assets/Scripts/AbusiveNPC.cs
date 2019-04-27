f6fafdbdec75c448f4d21377d638934b6b9b4561
MatthewWFrank <frankmat@oregonstate.edu>
2019-04-27 16:11:40 -0500

    Industrial Update
    +  NPC with 3 different insults in place
    + Insults are loaded up as different sprites each time Play is enabled.
    + One of the insults ("Press 'F' to interact.") triggers a fall trip if the player presses F
    
    - Insult changes need to be linked to respawn system eventually.
    - Trap execution needs to be associated with player proximity to trap

---
+++ Louie's Lament/Assets/Scripts/AbusiveNPC.cs
@@ -0,0 +1,98 @@
+﻿using System;
+//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class AbusiveNPC : MonoBehaviour
+{
+    private GameObject floorTrap;
+    private GameObject floorTrapPlug;
+    private GameObject abuseCanvas;
+
+    // CITATION:
+    // Getting sprites to change
+    // https://forum.unity.com/threads/need-to-get-a-sprite-using-its-name-from-assets-folder.377401/
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        System.Random random = new System.Random();
+        int randomSprite = random.Next(3);
+
+        switch (randomSprite)
+        {
+            case 0:
+                deployFloorTrap();
+                break;
+            case 1:
+                deployNapAbuse();
+                break;
+            case 2:
+                deployRedShirtAbuse();
+                break;
+        }
+    }
+
+    private void deployFloorTrap()
+    {
+        removeFloorTrapPlug();
+        enableFloorTrap();
+        updateCanvas(0);
+    }
+
+    private void deployNapAbuse()
+    {
+        disableFloorTrap();
+        deployFloorTrapPlug();
+        updateCanvas(1);
+    }
+
+    private void deployRedShirtAbuse()
+    {
+        disableFloorTrap();
+        deployFloorTrapPlug();
+        updateCanvas(2);
+    }
+
+    private void updateCanvas(int itemToDeploy)
+    {
+        abuseCanvas = GameObject.FindGameObjectWithTag("Abuse Canvas");
+        SpriteRenderer abuseCanvasSpriteRenderer = abuseCanvas.GetComponent<Renderer>() as SpriteRenderer;
+
+        switch (itemToDeploy)
+        {
+            case 0:
+                abuseCanvasSpriteRenderer.sprite = Resources.Load<Sprite>("Block_Interact");
+                break;
+            case 1:
+                abuseCanvasSpriteRenderer.sprite = Resources.Load<Sprite>("Block_RedShirt");
+                break;
+            case 2:
+                abuseCanvasSpriteRenderer.sprite = Resources.Load<Sprite>("Block_AfterNap");
+                break;
+        }
+    }
+
+    private void enableFloorTrap()
+    {
+        floorTrap = GameObject.FindGameObjectWithTag("Floor Trap");
+        floorTrap.SetActive(true);
+    }
+
+    private void disableFloorTrap()
+    {
+        floorTrap = GameObject.FindGameObjectWithTag("Floor Trap");
+        floorTrap.SetActive(false);
+    }
+
+    private void deployFloorTrapPlug()
+    {
+        // uhhh...instantiate it?
+    }
+
+    private void removeFloorTrapPlug()
+    {
+        floorTrapPlug = GameObject.FindGameObjectWithTag("Floor Trap PLUG");
+        Destroy(floorTrapPlug);
+    }
+}
