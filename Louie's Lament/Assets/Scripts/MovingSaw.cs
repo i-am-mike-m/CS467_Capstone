99d547045dd4182b9da531ae5039f99c3845fb0d
MatthewWFrank <frankmat@oregonstate.edu>
2019-05-18 23:04:03 -0500

    Industrial Level Update
    - Moving Saw object added
    - Moving Saw script added

---
+++ Louie's Lament/Assets/Scripts/MovingSaw.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class MovingSaw : MonoBehaviour
+{
+    Player player;
+    private bool headToRightSide = true;
+    [SerializeField] GameObject leftEdge;
+    [SerializeField] GameObject rightEdge;
+
+    // CITATION: Getting rotation and translation to work in conjunction
+    // https://answers.unity.com/questions/765699/simultaneously-translating-and-rotating-2d-sprite.html
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (headToRightSide)
+        {
+            gameObject.transform.Translate(Vector3.right * 9 * Time.deltaTime, Space.World);
+            gameObject.transform.Rotate(Vector3.back * 6);
+
+            if (hitRightEdge())
+            {
+                headToRightSide = false;
+            }
+        }
+        else
+        {
+           gameObject.transform.Translate(Vector3.left * 6 * Time.deltaTime, Space.World);
+           gameObject.transform.Rotate(Vector3.forward * 6);
+
+            if (hitLeftEdge())
+            {
+                headToRightSide = true;
+            }
+        }
+    }
+
+    private bool hitLeftEdge()
+    {
+        return gameObject.transform.position.x < leftEdge.transform.position.x;
+    }
+
+    private bool hitRightEdge()
+    {
+        return gameObject.transform.position.x > rightEdge.transform.position.x;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        player.PlayerDeath();
+    }
+}
