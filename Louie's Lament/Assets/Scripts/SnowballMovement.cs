a5dccb7ec5995392111f66b7194635c2fdc69951
Louie <louis.m.nicastro@gmail.com>
2019-05-19 20:40:03 -0400

    Update Ice level to include appearing snowballs, sound on snowball impact, fix branch to make sure player death, and prep final end prefab chandycane

--- Louie's Lament/Assets/Scripts/SnowballMovement.cs
+++ Louie's Lament/Assets/Scripts/SnowballMovement.cs
@@ -1,24 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SnowballMovement : MonoBehaviour
 {
+    Player player;
+    public AudioSource wompwomp;
+
+    public float distanceToFireAndShow;
+    private BoxCollider2D floorArea;
+    private Vector3 distanceToPlayer;
+    private GameObject playerObject;
+    private bool isMoving = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = FindObjectOfType<Player>();
+        wompwomp = GetComponent<AudioSource>();
+
+        floorArea = gameObject.GetComponent<BoxCollider2D>();
+        gameObject.GetComponent<Renderer>().enabled = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
         if (Input.GetKeyDown(KeyCode.C))
         {
             Debug.Log("cw");
             TurnCounterClockwise();
-        }
+        }*/
 
     }
 
@@ -30,11 +45,54 @@ public class SnowballMovement : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (isMoving)
+            Move();
+        else
+        {
+            distanceToPlayer = getDistanceToPlayer();
+            //Debug.Log("distance x: " + distanceToPlayer.x);
+            //Debug.Log("distance to display: " + distanceToFireAndShow);
+
+            if (distanceToPlayer.x < distanceToFireAndShow)
+            {
+                isMoving = true;
+                gameObject.GetComponent<Renderer>().enabled = true;
+
+            }
+        }
+
+
+
+    }
+
+    private void Move()
+    {
+
         //transform.position += transform.right * -2 * Time.deltaTime;
         transform.Translate(Vector3.left * Time.deltaTime * 3, Space.World);
         TurnCounterClockwise();
 
         //MoveLeft();
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Character")
+        {
+            wompwomp.Play();
+            player.PlayerDeath();
+
+        }
+    }
+
+    private Vector3 getDistanceToPlayer()
+    {
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+        distanceToPlayer = gameObject.transform.position - playerObject.transform.position;
+
+
+        return gameObject.transform.position - playerObject.transform.position;
 
     }
 
---
+++ Louie's Lament/Assets/Scripts/TreeBranchDeath.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeBranchDeath : MonoBehaviour
+{
+    Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Character")
+            player.PlayerDeath();
+    }
+}
